e563c22995dd28a08f655deb581e2c2e42d45f0a
S Steven Joshua <136824140+S-Steven-Joshua@users.noreply.github.com>
2026-06-12 22:50:33 +0530

    Add files via upload

---
+++ PWM_APB/pwm_core.sv
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.06.2026 23:54:22
+// Design Name: 
+// Module Name: pwm_core
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_core(
+    input logic clk,
+    input logic rst,
+    input logic [31:0] paddr,
+    input logic [31:0] data,
+    output logic wave
+    );
+    
+    logic psel;
+    logic trans;
+    logic pready;
+    logic pwrite;
+    logic [31:0] pwdata;
+    //logic stop;
+    
+    apb_master apb_master2(
+        .clk(clk),
+        .prstn(~rst),
+        .paddr(paddr),
+        .data(data),
+        .pready(pready),//input from the slave 
+        .psel(psel),
+        .penable(penable),
+        .pwrite(pwrite),
+        .pwdata(pwdata),
+        .trans(trans)
+    );
+    logic busy;
+    logic write;
+    logic [31:0] p_data;
+    apb_slave apb_slave1(
+        .clk(clk),
+        .prstn(~rst),
+        .psel(psel),
+        .trans(trans),
+        .pready_p(~busy),
+        .penable(penable),
+        .pwrite(pwrite),
+        .pwdata(pwdata),
+        .p_data(p_data),
+        .pready(pready),
+        .pwrite_en(write)
+    );
+    
+    pwm pwm1(
+        .clk(clk),
+        .rst(rst),
+        .data(p_data),
+        .write(write),
+        .wave(wave),
+        .busy(busy)
+    );
+endmodule:pwm_core
---
+++ PWM_APB/pwm_coretb.sv
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.06.2026 00:33:07
+// Design Name: 
+// Module Name: pwm_coretb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_coretb;
+    logic clk;
+    logic rst;
+    logic [31:0] paddr;
+    logic [31:0] data;
+    logic wave;
+    
+    pwm_core pwm_core1(
+        .clk(clk),
+        .rst(rst),
+        .paddr(paddr),
+        .data(data),
+        .wave(wave)
+    );
+    
+    initial begin 
+    clk=0;
+    forever #5 clk =~clk;
+    end
+    initial begin
+    $monitor(" psel=%b penable=%b trans=%b write=%b data=%h pready=%b p_data=%h pwdata=%h state=%d  busy=%b enable=%b",pwm_core.apb_slave1.psel,
+        pwm_core.apb_slave1.penable,pwm_core.apb_slave1.trans,pwm_core.apb_slave1.pwrite_en,data,
+        pwm_core.pwm1.busy,pwm_core.apb_slave1.p_data,pwm_core.apb_slave1.pwdata,pwm_core.apb_master2.state,pwm_core.pwm1.busy,
+        pwm_core.pwm1.enable
+        );
+    end
+    initial begin
+    rst=0;
+    #5;
+    rst=1;
+    #20;
+    rst=0;
+    
+    paddr=32'h4000_0008;
+    data=32'h000A_0003;
+    
+    #50;
+    paddr=32'h4000_0010;
+    data=32'h0000_0000;
+    #200;
+    paddr=32'h4000_000C;
+    data=32'h0000_0000;
+    #200;
+    
+    paddr=32'h4000_0008;
+    data=32'h000A_0003;
+    
+    #200;   
+    
+    $finish;
+    end
+
+endmodule:pwm_coretb
